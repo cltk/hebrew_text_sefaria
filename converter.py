8f258a4562061b6c7de15a2759cf06e50afb46c8
Luke Hollis <lukehollis@gmail.com>
2017-02-18 22:11:16 -0500

    update converter to remove empty nodes from the generated json documents and reconvert documents

--- converter.py
+++ converter.py
@@ -2,26 +2,38 @@ import os, re
 import json
 import pdb
 import collections
+from django.utils.text import slugify
 
 sourceLink = 'http://www.sefaria.org/'
 source = 'Sefaria'
 works = []
 
-def walkText(text):
-	node = { str(i): t for i, t in enumerate(text) }
-	node = collections.OrderedDict(sorted(node.items()))
+def jaggedListToDict(text):
+	textNodes = []
+	for t in text:
+		if not isinstance(t, int) and len(t):
+			textNodes.append(t)
+	node = { str(i): t for i, t in enumerate(textNodes) }
+	node = collections.OrderedDict(sorted(node.items(), key=lambda k: int(k[0])))
 	for child in node:
 		if isinstance(node[child], list):
-			node[child] = walkText(node[child])
+			if len(node[child]) == 1:
+				node[child] = node[child][0]
+			else:
+				node[child] = jaggedListToDict(node[child])
+
 	return node
 
 def main():
+	if not os.path.exists('cltk_json'):
+		os.makedirs('cltk_json')
+
 	# Build json docs from txt files
 	for root, dirs, files in os.walk("."):
 		path = root.split('/')
 		print((len(path) - 1) * '---', os.path.basename(root))
 		for fname in files:
-			if fname == 'merged.json':
+			if fname == 'merged.json' and 'Other' not in path:
 				with open(os.path.join(root, fname)) as f:
 					data = json.load(f)
 				work = {
@@ -41,14 +53,12 @@ def main():
 					print('language not identified--review manually', root, fname)
 
 
-				work['text'] = walkText(data['text'])
+				work['text'] = jaggedListToDict(data['text'])
 				works.append(work)
 
 	for work in works:
-		fname = work['source'] + '__' + work['englishTitle'] + '__' + work['language'] + '.json'
+		fname = slugify(work['source']) + '__' + slugify(work['englishTitle'][0:140]) + '__' + slugify(work['language']) + '.json'
 		fname = fname.replace(" ", "")
-		if not os.path.exists('cltk_json'):
-			os.makedirs('cltk_json')
 		with open('cltk_json/' + fname, 'w') as f:
 			json.dump(work, f)
 
